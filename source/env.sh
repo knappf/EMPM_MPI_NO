e2e4b80a0fa78b46fff04fba52f79cbf97c522cd
knapp <knapp@ipnp.troja.mff.cuni.cz>
2020-08-12 12:36:19 +0200

    scritpts update

---
+++ source/env.sh
@@ -0,0 +1,2 @@
+source /cvmfs/software.metacentrum.cz/software_basics/intel-parallel-studio/cluster/19.0.4/compilers_and_libraries_2019.4.243/linux/bin/compilervars.sh intel64 -platform linux
+source /cvmfs/software.metacentrum.cz/software_basics/intel-parallel-studio/cluster/19.0.4/compilers_and_libraries_2019.4.243/linux/mkl/bin/mklvars.sh intel64
--- source/modules.sh
+++ source/modules.sh
@@ -1,4 +1,4 @@
 # on Metacentrum load modules 
 module add intelcdk-19.0.4
-module add openmpi-1.8.2-intel
-module rm intelcdk-15
+#module add openmpi-1.8.2-intel
+#module rm intelcdk-15
